b8c4ee498b3058e9e74ac26cdcadf80ff882a4de
RobertAzarian <az.roberto1998@gmail.com>
2023-04-19 17:01:09 +0400

    Stage 3/5: Endless game

--- Main.java
+++ Main.java
@@ -13,25 +13,52 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        Option userOption = Option.valueOf(scanner.nextLine().toUpperCase());
-        Option compOption;
-        int randOption = random.nextInt(1, 4);
-        switch (randOption) {
-            case 1 -> compOption = Option.PAPER;
-            case 2 -> compOption = Option.SCISSORS;
-            default -> compOption = Option.ROCK;
-        }
-        String uNameOption = userOption.name().toLowerCase();
-        String cNameOption = compOption.name().toLowerCase();
+        do {
+            String input = scanner.nextLine();
 
-        if (userOption == compOption) {
-            System.out.printf("There is a draw (%s)\n", uNameOption);
-        } else if (userOption.wins.equals(cNameOption)) {
-            System.out.printf("Well done. The computer chose %s and failed\n", cNameOption);
-        } else if (userOption.loses.equals(cNameOption)) {
-            System.out.printf("Sorry, but the computer chose %s\n", cNameOption);
-        } else {
-            System.out.println("error");
+            int codeOfChecking = checkingInput(input);
+            if (codeOfChecking == 0) {
+                break;
+            } else if (codeOfChecking == 1) {
+                continue;
+            }
+
+            Option userOption = Option.valueOf(input.toUpperCase());
+            Option compOption;
+            int randOption = random.nextInt(1, 4);
+            switch (randOption) {
+                case 1 -> compOption = Option.PAPER;
+                case 2 -> compOption = Option.SCISSORS;
+                default -> compOption = Option.ROCK;
+            }
+
+            String uNameOption = userOption.name().toLowerCase();
+            String cNameOption = compOption.name().toLowerCase();
+
+            if (userOption == compOption) {
+                System.out.printf("There is a draw (%s)\n", uNameOption);
+            } else if (userOption.wins.equals(cNameOption)) {
+                System.out.printf("Well done. The computer chose %s and failed\n", cNameOption);
+            } else if (userOption.loses.equals(cNameOption)) {
+                System.out.printf("Sorry, but the computer chose %s\n", cNameOption);
+            } else {
+                System.out.println("error");
+            }
+        } while (true);
+    }
+
+    public static int checkingInput(String input) {
+        if (input.equals("!exit")) {
+            System.out.println("Bye!");
+            return 0;
         }
+
+        for (Option option : Option.values()) {
+            if (input.toUpperCase().equals(option.name())) {
+                return 2;
+            }
+        }
+        System.out.println("Invalid input");
+        return 1;
     }
 }
