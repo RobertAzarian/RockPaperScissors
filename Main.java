8a47b8369cd453cc586a8602756ef1661c3c809e
RobertAzarian <az.roberto1998@gmail.com>
2023-04-19 20:33:06 +0400

    Stage 4/5: Scoring the game

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 package rockpaperscissors;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+    static HashMap<String, Integer> playersBase = getRatingBaseFromTheFile();
+
     public static void main(String[] args) {
         game();
     }
@@ -13,13 +19,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
+        String nameOfUser;
+        int exitStatusName;
+        do {
+            System.out.print("Enter your name: ");
+            nameOfUser = scanner.nextLine();
+            exitStatusName = isNameExists(nameOfUser);
+            if (exitStatusName == 0) {
+                System.out.println("Buy!");
+                return;
+            }
+        } while (exitStatusName != 2);      // exitStatus: 0 - exit, 1 - false, 2 - true;
+
         do {
             String input = scanner.nextLine();
 
-            int codeOfChecking = checkingInput(input);
-            if (codeOfChecking == 0) {
-                break;
-            } else if (codeOfChecking == 1) {
+            int exitStatus = inputProcessing(input, nameOfUser);
+            if (exitStatus == 0) {
+                System.out.println("Bye!");
+                return;
+            } else if (exitStatus == 1) {
                 continue;
             }
 
@@ -37,8 +56,10 @@ public class Main {
 
             if (userOption == compOption) {
                 System.out.printf("There is a draw (%s)\n", uNameOption);
+                playersBase.replace(nameOfUser, playersBase.get(nameOfUser) + 50);
             } else if (userOption.wins.equals(cNameOption)) {
                 System.out.printf("Well done. The computer chose %s and failed\n", cNameOption);
+                playersBase.replace(nameOfUser, playersBase.get(nameOfUser) + 100);
             } else if (userOption.loses.equals(cNameOption)) {
                 System.out.printf("Sorry, but the computer chose %s\n", cNameOption);
             } else {
@@ -47,9 +68,31 @@ public class Main {
         } while (true);
     }
 
-    public static int checkingInput(String input) {
+
+
+    public static int isNameExists(String input) {
         if (input.equals("!exit")) {
-            System.out.println("Bye!");
+            return 0;
+        }
+
+        boolean isNameExists = false;
+        for (String nameFromBase : playersBase.keySet()) {
+            if (input.equals(nameFromBase)) {
+                isNameExists = true;
+                System.out.println("Hello, " + input);
+                break;
+            }
+        }
+        if (!isNameExists) {
+            System.out.println("Invalid name input");
+            return 1;
+        }
+        return 2;
+    }
+
+
+    public static int inputProcessing(String input) {
+        if (input.equals("!exit")) {
             return 0;
         }
 
@@ -60,5 +103,30 @@ public class Main {
         }
         System.out.println("Invalid input");
         return 1;
+    }
+
+
+    public static int inputProcessing(String input, String name) {
+        if ("!rating".equals(input)) {
+            int rating = playersBase.get(name);
+            System.out.println("Your rating: " + rating);
+        } else {
+            return inputProcessing(input);
+        }
+        return 1;
+    }
+
+
+    public static HashMap<String, Integer> getRatingBaseFromTheFile() {
+        HashMap<String, Integer> playersBase = new HashMap<>();
+        try (FileReader ratingFile = new FileReader("rating.txt")) {
+            Scanner scanFile = new Scanner(ratingFile);
+            while (scanFile.hasNextLine()) {
+                playersBase.put(scanFile.next(), scanFile.nextInt());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return playersBase;
     }
 }
